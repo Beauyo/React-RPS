ee8de0b67e70da55d8efe79f9d0302be8892bb3e
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-03-14 12:00:57 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Button, } from 'semantic-ui-react';
+
 
 class App extends Component {
+  state = { 
+    Rock: "rock",
+    Paper: "paper",
+    Scissors: "scissors"
+    }
+
+    userChoice = () => {
+    
+    }
+
+    handleClick = () => {
+      
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Button color="blue" onClick={() => this.userChoice() }>Rock</Button>
+        <Button color="blue" onClick={() => this.userChoice() }>Paper</Button>
+        <Button color="blue" onClick={() => this.userChoice() }>Scissors</Button>
+      </Container>
+   
     );
   }
 }
